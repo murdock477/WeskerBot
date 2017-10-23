ead56d6a95528a82629f67278c75a5f03db81f9c
unknown <martin.ilbi@gmail.com>
2017-10-23 23:07:42 +0300

    small stuff

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-###########
+import discord
+import logging
+
+#loogimise moodul
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    client = discord.Client
+    kasutajanimi = client.user('WeskerBot')
+    email = client.email('9gag@gmail.com')
+
+
+main()
