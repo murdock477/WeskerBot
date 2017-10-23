734692c685974a3a91b6a26f9e4540483ec8b1a5
murdock477 <martin.ilbi@gmail.com>
2017-10-24 00:40:35 +0300

    bot now ready to function (after watching some yt vids lol)

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import discord
 import logging
+import asyncio
 
+
+with open('token.txt', 'r') as token:
+    token = token.readline()
 #loogimise moodul
 logging.basicConfig(level=logging.INFO)
 
+client = discord.Client()
 
-def main():
-    client = discord.Client
-    kasutajanimi = client.user('WeskerBot')
-    email = client.email('9gag@gmail.com')
+@client.event #kirjutab yle client evendis
+
+async def on_ready():
+    print('logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-----------')
 
 
-main()
+@client.event
+
+async def on_message(message):
+    if message.content.startswith('/who is the best'):
+        await client.send_message(message.channel, 'Martin Ilbi')
+    elif message.content.startswith('/help'):
+        await client.send_message(message.channel, 
+        """
+        Showing all available commands:
+        1) /help
+        2) /who is the best
+        """)
+
+
+client.run(token)
