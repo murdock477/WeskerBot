de393483ac50f284bdb26c9bb72d38129b1c9d29
murdock477 <martin.ilbi@gmail.com>
2017-10-24 01:44:34 +0300

    added YT search and few other small commands

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import discord
 import logging
 import asyncio
+import requests
+from discord.ext import commands
 
 
+#t6mbab tokeni failist et github ei n2eks
 with open('token.txt', 'r') as token:
     token = token.readline()
 #loogimise moodul
 logging.basicConfig(level=logging.INFO)
-
+description = 'nibbabot'
 client = discord.Client()
+bot = commands.Bot(command_prefix='?', description=description)
 
 @client.event #kirjutab yle client evendis
 
@@ -22,15 +26,31 @@ async def on_ready():
 @client.event
 
 async def on_message(message):
-    if message.content.startswith('/who is the best'):
+    if message.content.startswith('/bestperson'):
         await client.send_message(message.channel, 'Martin Ilbi')
     elif message.content.startswith('/help'):
         await client.send_message(message.channel, 
         """
         Showing all available commands:
-        1) /help
-        2) /who is the best
+        1) /help - all commands
+        2) /bestperson - the best person on the planet
+        4) /yt + search term - useless youtube search
         """)
+    elif message.content.startswith('/yt'):
+        term = message.content.replace('/yt ', '')
+        url = 'https://www.youtube.com/results?search_query=' + term
+        await client.send_message(message.channel, url)
+
+
+@client.event
+async def on_message_delete(message):
+    sonum = '{0.author.name} kustutas ära sõnumi:\n{0.content}'
+    await client.send_message(message.channel, sonum.format(message))
+
+@bot.command()
+async def joined(member : discord.Member):
+    #ss kui keegi liitub discordi
+    await client.say('{0.name} liitus {0.joined_at}'.format(member))
 
 
 client.run(token)
